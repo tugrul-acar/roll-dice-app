98341ddb8736463a9e9fc6a8cc05692dfbe1effb
tugrul-acar <tugrul.acar.1905@gmail.com>
2024-07-15 02:14:04 +0300

    roll dice app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# roll dice app
- 
+Roll Dice App
+This is a simple Flutter app that allows users to roll a dice by pressing a button. 
---
+++ first_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:first_app/gradient_container.dart';
+
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Colors.deepOrange,
+          Colors.deepPurple,
+        ),
+      ),
+    ),
+  );
+}
